d6e4492af67759b7e5678d9a29758105d1b78dd7
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-03-25 11:29:49 +0000

    redirection using list of month names

--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -1,33 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 monthly = {
-    "jan": "No wokery here!",
-    "feb": "British and proud",
-    "mar": "Make Britain Great Again"
+    "jan": "Januaury is coolest",
+    "feb": "February is friendly",
+    "mar": "March can be rainy",
+    "apr": "April is a new life at Easter",
+    "may": "May is often pleasant when sunny",
+    "jun": "June is too hot in afternoon",
+    "jul": "July is often too hot in morning",
+    "aug": "August may be too hot in afternoon",
+    "sep": "September is usually pleasant"
 }
+
 
 # dict is ordered by default since Python>3.6
 def month_by_number(request, month):
-    return HttpResponse(month)
+    months = list(monthly.keys())
+    fwd_month = months[month]
+    return HttpResponseRedirect("/challenges/" + fwd_month)
+
 
 def month_by_name(request, month):
     try:
         msg = monthly[month]
         return HttpResponse(msg)
     except:
-        return HttpResponse("Month not recognised")
+        return HttpResponseNotFound("Month not recognised")
 
-def month_manual(request, month):
-    if month == "january":
-        msg = "Jan working!"
-    elif month == "february":
-        msg = "fish today"
-    else:
-        return HttpResponseNotFound("Not Found")
-    return HttpResponse(msg)
 
 def index(request):
     return HttpResponse("working!")
-def ifeb(request):
-    return HttpResponse("February content")
