26f2eb80f5a83759308fa0fb25b9d642b2c42447
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-03-28 19:57:46 +0000

    base template

--- mypage/mypage/settings.py
+++ mypage/mypage/settings.py
@@ -54,6 +54,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
+            BASE_DIR / "templates"
             # BASE_DIR / "challenges" / "templates"
         ],
         'APP_DIRS': True,
