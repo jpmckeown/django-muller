573bea8150516deadf5f8ee855cac5c322879ff3
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-03-30 16:43:41 +0100

    adjusting database schema and inserting data

--- my_site/store/models.py
+++ my_site/store/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 class Book(models.Model):
+    author = models.CharField(null=True, max_length=50)
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(null=True, validators=[MinValueValidator(1), MaxValueValidator(5)])
     # id = models.AutoField() # automatically added by Django
+
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
