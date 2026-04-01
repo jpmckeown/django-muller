8eb844cf81036afa01aa5aa52c0bc09f4737f9a9
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-04-01 20:23:52 +0100

    more my_site setup; also tidy store app

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -25,6 +25,7 @@ ALLOWED_HOSTS = []
 # Application definition
 INSTALLED_APPS = [
     'store',
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
