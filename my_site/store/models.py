5917cadcb64fdba37fcc80e4e3cc28ddc26fc570
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-04-01 15:45:16 +0100

    absolute_url, and slugify title for URL

--- my_site/store/models.py
+++ my_site/store/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-
+from django.urls import reverse
+from django.utils.text import slugify
 
 class Book(models.Model):
     author = models.CharField(null=True, max_length=50)
     title = models.CharField(max_length=50)
     rating = models.IntegerField(null=True, validators=[MinValueValidator(1), MaxValueValidator(5)])
-    # id = models.AutoField() # automatically added by Django
+    id = models.AutoField() # automatically added by Django
+    slug = models.SlugField()
 
+    def get_absolute_url(self):
+        return reverse("detail", args=[self.id])
+    # reverse("model_detail", kwargs={"pk": self.pk})
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+    
     def __str__(self):
         return f"{self.title} by {self.author} ({self.rating})"
