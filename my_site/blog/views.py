b69a3b6223e8be1354df3d9273106d4a1b4d8b79
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-04-01 20:24:24 +0100

    begin blog app urls views template

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+# a view must return an HttpResponse not a string
+def index(request):
+    return HttpResponse('index')
+
+def post(request):
+    return HttpResponse('one post')
