20bdc10c298ba246dd3664fe7ba7384222273635
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-04-02 20:26:44 +0100

    onepost setup; and inline data

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='splash'),
     path('blog/', views.allposts, name='blogindex'),
-    path('blog/<slug:slug>', views.post, name='onepost')
+    path('blog/<slug:slug>', views.onepost, name='onepost')
 ]
