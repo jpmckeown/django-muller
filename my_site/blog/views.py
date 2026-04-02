9df03294a075859108015211fee0bcaa669fea2c
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-04-02 14:37:32 +0100

    wip setup separate css for project and app

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 
 # a view must return an HttpResponse not a string
 def index(request):
-    return HttpResponse('index')
+    return render(request, 'blog/index.html')
+    # return HttpResponse('index')
 
 def post(request):
     return HttpResponse('one post')
--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -49,7 +49,9 @@ ROOT_URLCONF = 'my_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,4 +111,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
