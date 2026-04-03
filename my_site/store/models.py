4f701029392134b639a4df3acb11cb8122e18a1e
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-04-03 07:43:55 +0100

    title save with Title Case except little words

--- my_site/store/models.py
+++ my_site/store/models.py
@@ -8,22 +8,33 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(null=True, max_length=50)
     rating = models.IntegerField(null=True, validators=[MinValueValidator(1), MaxValueValidator(5)])
-    slug = models.SlugField(default='', null=False, blank=True, editable=False)
+    slug = models.SlugField(default='', null=False, blank=True)
     # id = models.AutoField() # automatically added by Django
+
+    _TITLE_CASE_EXCEPTIONS = {
+        'a', 'an', 'the', 'and', 'but', 'or', 'nor', 'for', 'so', 'yet',
+        'at', 'by', 'in', 'of', 'on', 'to', 'up', 'as',
+    }
 
     @staticmethod
     def slugify_skip_the(title: str) -> str:
         stripped = re.sub(r'^the\s+', '', title, flags=re.IGNORECASE)
         return slugify(stripped)
-    
+
+    def apply_title_case(self, title: str) -> str:
+        words = title.split()
+        return ' '.join(
+            word.capitalize() if i == 0 or word.lower() not in self._TITLE_CASE_EXCEPTIONS
+            else word.lower()
+            for i, word in enumerate(words)
+        )
+
     def get_absolute_url(self):
         return reverse("detail", args=[self.slug])
-    # reverse("model_detail", kwargs={"pk": self.pk})
 
     def save(self, *args, **kwargs):
-        self.slug = self.slugify_skip_the(self.title)
-        # self.slug = slugify(self.title)
+        self.title = self.apply_title_case(self.title)
         super().save(*args, **kwargs)
     
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.rating}) {self.slug}"
+        return f"{self.title}, by {self.author}. ({self.rating}) {self.slug}"
