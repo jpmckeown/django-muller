89bd2be8d1ef31b694adbf3955e797f043d14192
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-04-03 08:03:04 +0100

    filter by author at admin listing

--- my_site/store/admin.py
+++ my_site/store/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
+    list_filter = ('author',)
     class Media:
         js = ('store/admin_book.js',)
+    
     # prepopulated_fields = {'slug': ('title',)}  # replaced by custom JS (strips leading "the")
     # readonly_fields = ('slug',)  # earlier approach but if readonly cannot prepopulate
 
