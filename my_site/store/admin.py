b4291c348396653456df54fe87c6ca55fe971f11
McKeown <mckeownj@conted.ox.ac.uk>
2026-04-07 14:37:38 +0100

    author model adjust

--- my_site/store/admin.py
+++ my_site/store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author
 
 class BookAdmin(admin.ModelAdmin):
     list_filter = ('author', 'rating',)
@@ -12,3 +12,4 @@ class BookAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
--- my_site/store/models.py
+++ my_site/store/models.py
@@ -9,8 +9,14 @@ class Author(models.Model):
     first_name = models.CharField(max_length=80)
     last_name = models.CharField(max_length=80)
 
-    def __str__(self):
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def last_name_first(self):
         return f"{self.last_name}, {self.first_name}"
+
+    def __str__(self):
+        return self.last_name_first()
 
 
 class Book(models.Model):
