588d638d0da7de29b6e10e9c3a0add1071d8cc48
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-04-09 11:16:34 +0100

    begin tables for blog to replace internal data

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    first = models.CharField(max_length=50)
+    last = models.CharField(max_length=50)
+    email = models.EmailField(max_length=70)
+
+    def __str__(self):
+        return f"{self.first} {self.last} {self.email}"
+
+class Post(models.Model):
+    title = models.CharField(max_length=80)
+    excerpt = models.CharField(max_length=120)
+    image = models.CharField(max_length=80)
+    date = models.DateField()
+    slug = models.CharField(max_length=80)
+    content = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f"{self.title}"
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=40)
+
