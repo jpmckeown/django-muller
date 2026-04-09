38f622b5ac93c81f3ca9dfa11b2e2211244d2981
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-04-09 14:09:03 +0100

    models revised to match tutorial

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Post
+from .models import Author, Post, Tag
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title',)
 
+admin.site.register(Author)
 admin.site.register(Post)
+admin.site.register(Tag)
--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,24 +1,29 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
 
 class Author(models.Model):
     first = models.CharField(max_length=50)
     last = models.CharField(max_length=50)
-    email = models.EmailField(max_length=70)
+    email = models.EmailField(max_length=80)
 
     def __str__(self):
         return f"{self.first} {self.last} {self.email}"
 
-class Post(models.Model):
-    title = models.CharField(max_length=80)
-    excerpt = models.CharField(max_length=120)
-    image = models.CharField(max_length=80)
-    date = models.DateField()
-    slug = models.CharField(max_length=80)
-    content = models.CharField(max_length=250)
-
-    def __str__(self):
-        return f"{self.title}"
 
 class Tag(models.Model):
     caption = models.CharField(max_length=40)
 
+
+class Post(models.Model):
+    title = models.CharField(max_length=80)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=80)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True, max_length=80)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(to=Author, on_delete=models.SET_NULL, related_name="posts")
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f"{self.title}"
