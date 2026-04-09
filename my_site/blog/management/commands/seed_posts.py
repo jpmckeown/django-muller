f79f931681c10c90a01252a070c266142435acc4
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-04-09 14:31:07 +0100

    delete indent from imported content lines

--- my_site/blog/management/commands/seed_posts.py
+++ my_site/blog/management/commands/seed_posts.py
@@ -1,17 +1,16 @@
 from django.core.management.base import BaseCommand
 from blog.models import Post
 from datetime import date
+import textwrap
 
 posts = [
     {
         "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Maximilian",
+        "image_name": "mountains.jpg",
         "date": date(2026, 3, 21),
         "title": "Mountain Hiking",
         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+        "content": textwrap.dedent("""Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
@@ -22,17 +21,15 @@ posts = [
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
+        """)
     },
     {
         "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian",
+        "image_name": "coding.jpg",
         "date": date(2022, 3, 10),
         "title": "Programming Is Great!",
         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+        "content": textwrap.dedent("""Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
@@ -43,17 +40,15 @@ posts = [
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
+        """)
     },
     {
         "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Maximilian",
+        "image_name": "woods.jpg",
         "date": date(2020, 8, 5),
         "title": "Nature At Its Best",
         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+        "content": textwrap.dedent("""Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
@@ -64,12 +59,13 @@ posts = [
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
+        """)
     }
 ]
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
+        # for p in posts:
+        #     Post.objects.get_or_create(slug=p["slug"], defaults=p)
         Post.objects.bulk_create([Post(**p) for p in posts])
- 
  
