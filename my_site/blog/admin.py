ed05d3a48dde2f3922502084188a44a48e3d5325
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-04-10 06:16:53 +0100

    migration for tag manytomany field

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Author, Post, Tag
 
+class AuthorAdmin(admin.ModelAdmin):
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title',)
+
+CLASS
 
 admin.site.register(Author)
 admin.site.register(Post)
