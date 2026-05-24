2ba28cdfce72e227e033b80d80a49a1da85c4f9e
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-05-24 06:41:14 +0100

    Forms views.py urls.py and a Thanks after submit

--- useforms/feedback/urls.py
+++ useforms/feedback/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.feedback)
+    path("", views.feedback),
+    path("thanks/", views.thanks)
 ]
--- useforms/feedback/views.py
+++ useforms/feedback/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 def feedback(request):
+    if request.method == 'POST':
+        entered_username = request.POST['username']
+        print(entered_username)
+        return HttpResponseRedirect("/thanks/")
+    
     return render(request, 'feedback/feedback.html')
+
+def thanks(request):
+    return render(request, 'feedback/thanks.html')
