7cf295ffd12ea5b641cdc824664e2ba5c4bc7100
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-05-25 12:19:41 +0100

    begin validation for empty form on submit

--- useforms/feedback/views.py
+++ useforms/feedback/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
+
 def feedback(request):
     if request.method == 'POST':
+        # request holds a dictionary
         entered_username = request.POST['username']
+
+        if entered_username=="":
+            return render(request, 'feedback/feedback.html', {"has_error": True})
+
         print(entered_username)
         return HttpResponseRedirect("/thanks/")
     
     return render(request, 'feedback/feedback.html')
 
+
 def thanks(request):
-    return render(request, 'feedback/thanks.html')
+    return render(request, 'feedback/thanks.html', {"has_error": False})
