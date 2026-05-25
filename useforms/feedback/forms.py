527bf4cb86547f32a6d1b26e7acf5407149f68d7
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-05-25 20:00:38 +0100

    customise form display in template, also a style only if errors

--- useforms/feedback/forms.py
+++ useforms/feedback/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-    user_name_form = forms.CharField(label="Your name", max_length=30, error_messages={
+    user_name = forms.CharField(label="Your name", max_length=30, error_messages={
         "required": "Your username should not be blank.",
         "max_length": "Your username should not exceed 20 characters length.",
     })
