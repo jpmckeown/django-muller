484c675c8fa7fe8038495da2e48087a6182ee801
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-05-26 20:52:35 +0100

    using model to save to database

--- useforms/feedback/views.py
+++ useforms/feedback/views.py
@@ -1,40 +1,52 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
-from .forms import ReviewForm 
+from .forms import ReviewForm
+from .models import Review
+
 
 # Forms class way
 def feedback(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ReviewForm(request.POST)
 
         if form.is_valid():
             print(form.cleaned_data)
+            review = Review(
+                user_name=form.cleaned_data["user_name"],
+                review_text=form.cleaned_data["review_text"],
+                rating=form.cleaned_data["rating"],
+            )
+            review.save()
             return HttpResponseRedirect("/thanks/")
 
     else:
         # make new empty form
         form = ReviewForm()
 
-    return render(request, 'feedback/feedback.html', {
-        "form": form,
-    })
+    return render(
+        request,
+        "feedback/feedback.html",
+        {
+            "form": form,
+        },
+    )
 
 
 # manual way
 def feedback_manual(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         # request holds a dictionary
-        entered_username = request.POST['username']
+        entered_username = request.POST["username"]
 
-        if entered_username=="":
-            return render(request, 'feedback/feedback.html', {"has_error": True})
+        if entered_username == "":
+            return render(request, "feedback/feedback.html", {"has_error": True})
 
         print(entered_username)
         return HttpResponseRedirect("/thanks/")
-    
-    return render(request, 'feedback/feedback.html', {"has_error": False})
+
+    return render(request, "feedback/feedback.html", {"has_error": False})
 
 
 def thanks(request):
-    return render(request, 'feedback/thanks.html')
+    return render(request, "feedback/thanks.html")
