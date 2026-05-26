73267c4cc603135ef28fb2d38768374e825597c2
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-05-26 21:07:17 +0100

    use ModelForm instead of Form with specified fields

--- useforms/feedback/forms.py
+++ useforms/feedback/forms.py
@@ -1,9 +1,17 @@
 from django import forms
+from .models import Review
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(label="Your name", max_length=30, error_messages={
-        "required": "Your username should not be blank.",
-        "max_length": "Your username should not exceed 20 characters length.",
-    })
-    review_text = forms.CharField(label="Feedback", widget=forms.Textarea, max_length=200)
-    rating = forms.IntegerField(label="Rating", min_value=1, max_value=5)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label="Your name", max_length=30, error_messages={
+#         "required": "Your username should not be blank.",
+#         "max_length": "Your username should not exceed 20 characters length.",
+#     })
+#     review_text = forms.CharField(label="Feedback", widget=forms.Textarea, max_length=200)
+#     rating = forms.IntegerField(label="Rating", min_value=1, max_value=5)
--- useforms/feedback/models.py
+++ useforms/feedback/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+
 class Review(models.Model):
     user_name = models.CharField(max_length=40)
     review_text = models.CharField(max_length=200)
     rating = models.IntegerField()
+    # secret = models.CharField(max_length=10)
