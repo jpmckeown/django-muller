04feb1dc9de6a6825b666f61e9c58b44c57c1e5c
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-06-01 14:12:29 +0100

    tests for feedback app

--- useforms/feedback/tests.py
+++ useforms/feedback/tests.py
@@ -1,3 +1,56 @@
 from django.test import TestCase
+from .forms import ReviewForm
 
-# Create your tests here.
+
+class ReviewFormTest(TestCase):
+    def test_valid_form(self):
+        form = ReviewForm(
+            data={
+                "user_name": "Alice",
+                "review_text": "Great service",
+                "rating": 4,
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_missing_user_name(self):
+        form = ReviewForm(
+            data={
+                "user_name": "",
+                "review_text": "Great service",
+                "rating": 4,
+            }
+        )
+        self.assertFalse(form.is_valid())
+        self.assertIn("user_name", form.errors)
+
+    def test_custom_error_message(self):
+        form = ReviewForm(
+            data={
+                "user_name": "",
+                "review_text": "Great service",
+                "rating": 4,
+            }
+        )
+        form.is_valid()
+        self.assertIn("dont be missing!", form.errors["user_name"])
+
+    def test_missing_review_text(self):
+        form = ReviewForm(
+            data={
+                "user_name": "Alice",
+                "review_text": "",
+                "rating": 4,
+            }
+        )
+        self.assertFalse(form.is_valid())
+
+    def test_missing_rating(self):
+        form = ReviewForm(
+            data={
+                "user_name": "Alice",
+                "review_text": "Great service",
+                "rating": "",
+            }
+        )
+        self.assertFalse(form.is_valid())
