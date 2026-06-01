f66a3a5e8f96d37b75b806fdef1c28bd857e4ac5
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-06-01 21:33:09 +0100

    View and url for Contact

--- valentino_testing/library/views.py
+++ valentino_testing/library/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from .models import Contact
+from .forms import ContactForm
 
-# Create your views here.
+
+class ContactView(CreateView):
+    model = Contact
+    form_class = ContactForm
+    template_name = "library/contact.html"
+    success_url = "/thanks/"
+
+
+class ThanksView(TemplateView):
+    template_name = "library/thanks.html"
