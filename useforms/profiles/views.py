344e204167cb601d7f5c7551c573d41563a81892
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-06-04 13:33:46 +0100

    continue file upload fixed file name format way

--- useforms/profiles/views.py
+++ useforms/profiles/views.py
@@ -3,10 +3,17 @@ from django.views import View
 from django.http import HttpResponseRedirect
 
 
+def store_file(file):
+    with open("temp/pic.png", "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+
+
 class CreateProfileView(View):
     def get(self, request):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
+        store_file(request.FILES["image"])
         print(request.FILES["image"])
         return HttpResponseRedirect("/profiles")
