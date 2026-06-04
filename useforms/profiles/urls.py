aa776a9ac33c8e6d9b6a80b0aa92d04a9192586d
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-06-04 19:21:09 +0100

    begin using ListView to show all uploaded images

--- useforms/profiles/urls.py
+++ useforms/profiles/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("profiles/", views.CreateProfileView.as_view())]
+urlpatterns = [
+    path("profiles/", views.CreateProfileView.as_view()),
+    path("profiles/list", views.ListProfilesView.as_view()),
+]
--- useforms/profiles/views.py
+++ useforms/profiles/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
-from django.views.generic import CreateView
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 
 from .forms import ProfileForm
 from .models import UserProfile
+
+
+class ListProfilesView(ListView):
+    template_name = "profiles/user_profiles.html"
+    model = UserProfile
+    context_object_name = "profiles"
 
 
 class CreateProfileView(CreateView):
