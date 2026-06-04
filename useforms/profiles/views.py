5de9b33580aca438ca4bd3cb51c6efb2ea2e2142
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-06-04 19:12:14 +0100

    CreateView shorter class-based View

--- useforms/profiles/views.py
+++ useforms/profiles/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.views.generic import CreateView
 
 from .forms import ProfileForm
 from .models import UserProfile
 
 
-class CreateProfileView(View):
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
+
+
+class CreateProfileView_usingForm(View):
     def get(self, request):
         form = ProfileForm
         # form = self.form()
