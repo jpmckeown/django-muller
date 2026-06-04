93f575bf3179e65863e066b28a759b82b67cf86c
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-06-04 16:03:44 +0100

    View def POST using Form class

--- useforms/profiles/views.py
+++ useforms/profiles/views.py
@@ -3,12 +3,7 @@ from django.views import View
 from django.http import HttpResponseRedirect
 
 from .forms import ProfileForm
-
-
-def store_file(file):
-    with open("temp/pic.png", "wb+") as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
+from .models import UserProfile
 
 
 class CreateProfileView(View):
@@ -18,9 +13,21 @@ class CreateProfileView(View):
         return render(request, "profiles/create_profile.html", {"form": form})
 
     def post(self, request):
-        store_file(request.FILES["image"])
-        print(request.FILES["image"])
-        return HttpResponseRedirect("/profiles")
+        submitted_form = ProfileForm(request.POST, request.FILES)
+        if submitted_form.is_valid():
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
+            # store_file(request.FILES["image"])
+            # print(request.FILES["image"])
+            return HttpResponseRedirect("/profiles")
+
+        return render(request, "profiles/create_profile.html", {"form": submitted_form})
+
+
+def store_file(file):
+    with open("temp/pic.png", "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
 
 
 class CreateProfileViewManually(View):
